daa9303e7c90bca5045b211218f001f1bab69e96
ryuya kaji <kaji.777r@gmail.com>
2023-05-15 04:04:45 +0900

    add: spasのカラム追加、csvファイルによるデータの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_13_155644) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_14_185723) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_13_155644) do
     t.text "spa_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "message"
+    t.string "family_bath_info"
     t.index ["name"], name: "index_spas_on_name", unique: true
   end
 
