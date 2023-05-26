14a85e37e7412ee4b52eefa1bca5741f6afe9e9f
ryuya kaji <kaji.777r@gmail.com>
2023-05-27 00:48:35 +0900

    add: searchコントローラ

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -1,4 +1,17 @@
 class QuestionController < ApplicationController
+
+  def question
+    @spas = Spa.all
+  end
+
+  def search
+    spa_quality_value = params[:spa_quality]
+    have_family_bath_value = params[:have_family_bath]
+    place_value = params[:place]
+
+    @results = Spa.where(spa_quality: spa_quality_value, have_family_bath: have_family_bath_value, place: place_value).order("RANDOM()").limit(3)
+  end
+
   def index
   end
 end
