8c697e806f83eba0dd2531a1ed43c06c0dc082f9
ryuya kaji <kaji.777r@gmail.com>
2023-06-27 23:28:58 +0900

    add: ログインログアウト機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -34,8 +34,8 @@ Rails.application.routes.draw do
 
   resources :spas, only: %i[index show]
 
-  get 'login', to: 'user_session#new'
-  post 'login', to: 'user_session#create'
-  get 'logout', to: 'user_session#delete'
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  get 'logout', to: 'user_sessions#destroy'
   resources 'users', only: %i[new create]
 end
