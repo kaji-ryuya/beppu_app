4e1259077957c75381b0e8ee013e1bd79b16a12b
ryuya kaji <kaji.777r@gmail.com>
2023-06-29 18:14:19 +0900

    gem configの実装

--- Gemfile
+++ Gemfile
@@ -67,6 +67,8 @@ gem 'ransack'
 gem 'sorcery'
 gem 'letter_opener_web'
 
+gem 'config'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.delivery_method = :letter_opener_web
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = Setting.default_url_options.to_h
 end
