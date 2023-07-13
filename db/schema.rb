c2bb83a3c9b0d408eda1426459f659e34d560589
ryuya kaji <kaji.777r@gmail.com>
2023-07-14 03:48:26 +0900

    add: ブルートフォース攻撃対策の実装（sorcery）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_13_130500) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_13_183510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,8 +63,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_13_130500) do
     t.datetime "reset_password_email_sent_at"
     t.integer "access_count_to_reset_password_page", default: 0
     t.integer "role", default: 0, null: false
+    t.integer "failed_logins_count", default: 0
+    t.datetime "lock_expires_at"
+    t.string "unlock_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
+    t.index ["unlock_token"], name: "index_users_on_unlock_token"
   end
 
   add_foreign_key "bookmarks", "spas"
